da375390b6e5911cbec9b851ee4c2185e5af1be3
arie.karlen <ariel.karlen@possumus.tech>
2023-07-24 10:24:13 -0300

    List News

--- components/featuredNews.js
+++ components/featuredNews.js
@@ -1,25 +1,25 @@
-import { Row, Image, Col } from "react-bootstrap";
+import { Row, Image, Col, Container } from "react-bootstrap";
 import featuredImage from "../assets/newsImage/featured.jpg";
 import moment from "moment";
 import "moment/locale/es";
 
 export default function FeaturedNews({ dataNews }) {
+    console.log(dataNews);
     return (
-        <>
-            {dataNews.map((data) => (
-                <>
-                    {data.attributes.Destacada == true && (
-                        <Row id="featuredNews">
-                            <Image src={featuredImage.src} fluid />
+        <Container id="featuredNews">
+            {dataNews.map(
+                (data) =>
+                    data.attributes.Destacada == true && (
+                        <Row>
+                            <Image src={data.attributes.Imagen_Destacada?.data.attributes.url} fluid />
                             <Col className="textContent">
                                 <h6>{moment(data.attributes.publishedAt).format("DD MMMM YYYY")}</h6>
                                 <h1>{data.attributes.Titulo}</h1>
                                 <p>{data.attributes.Bajada}</p>
                             </Col>
                         </Row>
-                    )}
-                </>
-            ))}
-        </>
+                    )
+            )}
+        </Container>
     );
 }
