892fb7a8767ba1999a7c9fcc53a7d8f01ab6eebb
arie.karlen <ariel.karlen@possumus.tech>
2023-08-11 14:59:05 -0300

    Add downloa pdf and tags

---
+++ components/webinars/downloadPdf.js
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Form, Button, Alert, Image, Row } from "react-bootstrap";
+import axios from "axios";
+
+export default function DownloadPdf({ file, source }) {
+    const [success, setSuccess] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
+    } = useForm({
+        mode: "onTouched",
+        defaultValues: {
+            origin: "Webinar",
+            source: source,
+            yourname: "",
+            enterprise: "",
+            email: "",
+            phone: "",
+        },
+    });
+
+    const onSubmit = (data) => {
+        axios.post(`https://prod-20.brazilsouth.logic.azure.com:443/workflows/4a0de618d958419bb7dc99ba7a3245b7/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=QsAL47KtZeSa5He1YXBvXMI4ZsD802JoaITVlC2wXhs`, data).then(() => {
+            window.open(file, "_ blank");
+            setSuccess(true);
+        });
+    };
+
+    return (
+        <>
+            {success ? (
+                <Row>
+                    <h1>Muchas gracias</h1>
+                    <p>La nota completa se abrirá en una nueva pestaña</p>
+                    <p>
+                        En caso de que esto no suceda, por favor haga{" "}
+                        <a href={file} target="_blank">
+                            click aquí
+                        </a>
+                    </p>
+                </Row>
+            ) : (
+                <form onSubmit={handleSubmit(onSubmit)} id="formDownloadWebinars">
+                    <Form.Control
+                        type="text"
+                        className="formField"
+                        placeholder="Nombre y apellido"
+                        aria-describedby="passwordHelpBlock"
+                        {...register("yourname", {
+                            required: {
+                                value: true,
+                                message: "Por favor, ingrese su nombre",
+                            },
+                        })}
+                    />
+                    {errors.yourname && <Alert variant="danger">{errors?.yourname?.message}</Alert>}
+                    <Form.Control
+                        type="text"
+                        className="formField"
+                        placeholder="Empresa"
+                        aria-describedby="passwordHelpBlock"
+                        {...register("enterprise", {
+                            required: {
+                                value: true,
+                                message: "Por favor, ingrese la organización a la que pertenece",
+                            },
+                        })}
+                    />
+                    {errors.enterprise && <Alert variant="danger">{errors?.enterprise?.message}</Alert>}
+                    <Form.Control
+                        type="text"
+                        className="formField"
+                        placeholder="Email"
+                        aria-describedby="passwordHelpBlock"
+                        {...register("email", {
+                            required: {
+                                value: true,
+                                message: "El email es obligatorio",
+                            },
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "Email no es valido",
+                            },
+                        })}
+                    />
+                    {errors.email && <Alert variant="danger">{errors?.email?.message}</Alert>}
+                    <Form.Control
+                        type="text"
+                        className="formField"
+                        placeholder="Teléfono"
+                        aria-describedby="passwordHelpBlock"
+                        {...register("phone", {
+                            required: {
+                                value: true,
+                                message: "Por favor, ingrese su telefono",
+                            },
+                        })}
+                    />
+                    {errors.phone && <Alert variant="danger">{errors?.phone?.message}</Alert>}
+                    <Button variant="primary" type="Submit" id="sendForm">
+                        Descargar
+                    </Button>
+                </form>
+            )}
+        </>
+    );
+}
--- pages/webinars/[slug].js
+++ pages/webinars/[slug].js
@@ -15,7 +15,7 @@ export async function getServerSideProps(context) {
     const { params } = context;
     const { slug } = params;
 
-    const URL = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_WEBINARS}?filters[slug][$eq]=${slug}&populate=Imagen_Destacada%2C%20imagen_header%2C%20webinars_tipo%2C%20populate=autores%2C%20autores.Imagen%2C%20autores.Perfiles%2C%20autores.puesto%2C%20cta%2C%20cta.Image_Background%2C%20Form_inscripcion%2C%20PostWebinar%2C%20PostWebinar.Imagen&sort=id:desc`;
+    const URL = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_WEBINARS}?filters[slug][$eq]=${slug}&populate=Imagen_Destacada%2C%20imagen_header%2C%20webinars_tipo%2C%20populate=autores%2C%20autores.Imagen%2C%20autores.Perfiles%2C%20autores.puesto%2C%20cta%2C%20cta.Image_Background%2C%20Form_inscripcion%2C%20PostWebinar%2C%20PostWebinar.Imagen%2C%20PostWebinar.background_form%2C%20PostWebinar.descarga%2C%20tags&sort=id:desc`;
     const URLINSTITUCIONAL = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_INSTITUTIONAL}?populate[0]=Contacto&populate[1]=Assets.Logo_Alt`;
 
     const res = await fetch(URL);
@@ -33,15 +33,15 @@ export default function fullNews({ dataNew, dataIns }) {
     return (
         <>
             <Head>
-                <title>Blog Possumus || {dataNew.data[0].attributes.Titulo}</title>
+                <title>Webinars Possumus || {dataNew.data[0].attributes.Titulo}</title>
                 <meta name="description" content={dataNew.data[0].attributes.Bajada} />
-                {/* <meta name="keywords" content={dataNew.data[0].attributes?.tags.data.map((data) => " " + data.attributes.Tag)} />
+                <meta name="keywords" content={dataNew.data[0].attributes?.tags.data.map((data) => " " + data.attributes.Tag)} />
                 <meta name="author" content="Possumus" />
                 <meta property="og:title" content={dataNew.data[0].attributes.Titulo} key="title" />
                 <meta property="og:description" content={dataNew.data[0].attributes.Bajada} />
                 <meta property="og:type" content="article" />
-                <meta property="og:site_name" content="Blog Possumus" />
-                <meta property="og:image" content={dataNew.data[0].attributes.Imagen_Destacada.data.attributes.url}></meta> */}
+                <meta property="og:site_name" content="Webinars Possumus" />
+                <meta property="og:image" content={dataNew.data[0].attributes.Imagen_Destacada.data.attributes.url}></meta>
             </Head>
             <Header title="Webinars" style="mainNavNoMargin" />
             <HeaderWebinar
@@ -58,7 +58,7 @@ export default function fullNews({ dataNew, dataIns }) {
             <Description text={dataNew.data[0].attributes.descripcion} />
             <Speakers speakers={dataNew.data[0].attributes.autores.data} />
             <Cta data={dataNew.data[0].attributes.cta} />
-            <PostWebinar status={dataNew.data[0].attributes.Destacada} data={dataNew.data[0].attributes.PostWebinar} />
+            <PostWebinar status={dataNew.data[0].attributes.Destacada} data={dataNew.data[0].attributes.PostWebinar} color={dataNew.data[0].attributes.color} />
             <Forms status={dataNew.data[0].attributes.Destacada} dataForm={dataNew.data[0].attributes.Form_inscripcion} source={dataNew.data[0].attributes.Titulo} title={dataNew.data[0].attributes.Titulo} subTitle={dataNew.data[0].attributes.Bajada} />
 
             <Footer dataInstitutional={dataIns} />
