7e6cc7099cadab87606eab782daa1f87cc4c522c
Agustin Vera <agustin.vera@possumus.tech>
2024-01-23 16:40:07 -0300

    feat: move CopyToClipboard function to utils

---
+++ utils/functions.js
@@ -0,0 +1,15 @@
+import { toast } from "sonner";
+
+export const CopyToClipboard = (toCopy, message = "Link copiado al portapapeles") => {
+  const el = document.createElement(`textarea`);
+  el.value = toCopy;
+  el.setAttribute(`readonly`, ``);
+  el.style.position = `absolute`;
+  el.style.left = `-9999px`;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand(`copy`);
+  document.body.removeChild(el);
+  console;
+  toast.success(message);
+};   
