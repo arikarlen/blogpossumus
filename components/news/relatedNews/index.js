c7f7d54a907c86e34c211dafc05bb5c8a51d155a
Agustin Vera <agustin.vera@possumus.tech>
2024-01-25 16:44:43 -0300

    add: relatedNews cmpt with his styles, skeleton...
    Lazy load and NoRelatedNewsFounded component to handle when we dont found related news in the actual new

---
+++ components/news/relatedNews/index.js
@@ -0,0 +1,32 @@
+import React, { Suspense, useRef } from "react";
+import useNearScreen from "../../../hooks/useNearScreen";
+import RelatedNewsSkeleton from "./relatedNewsSkeleton";
+import { Col, Row } from "react-bootstrap";
+import styles from "./relatedNews.module.css";
+
+const RelatedNews = React.lazy(() => import("./relatedNews"));
+
+export default function LazyRelatedNews({ tags, title, actualNewTitle }) {
+  const { isElementNearScreen, elementToObserveRef } = useNearScreen({
+    distanceToElementToObserve: "0px",
+  });
+
+  return (
+    <div ref={elementToObserveRef}>
+      <Row className={styles.container}>
+        <Col md={{ span: 8, offset: 2 }}>
+          <h3>{title}</h3>
+          <Row className={styles.grid}>
+            <Suspense fallback={<RelatedNewsSkeleton />}>
+              {isElementNearScreen ? (
+                <RelatedNews tags={tags} actualNewTitle={actualNewTitle}/>
+              ) : (
+                <RelatedNewsSkeleton />
+              )}
+            </Suspense>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
+}
---
+++ components/news/relatedNews/relatedNewsSkeleton.js
@@ -0,0 +1,10 @@
+import NoteSkeleton from "../../commons/note/NoteSkeleton";
+
+export default function RelatedNewsSkeleton() {
+  return (
+    <>
+      <NoteSkeleton />
+      <NoteSkeleton />
+    </>
+  );
+}
