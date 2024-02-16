f502af80578339708082c2b0b70f1ff481e828c2
Agustin Vera <agustin.vera@possumus.tech>
2024-02-16 14:04:10 -0300

    fix: refactoring index to use fetcher util

--- pages/index.js
+++ pages/index.js
@@ -5,68 +5,92 @@ import ListNews from "../components/news/blogList";
 import Footer from "../components/commons/footer/footer";
 import Analitycs from "../components/commons/analitycs";
 import Title from "../components/commons/titles";
-import SeeMoreeButton from "../components/commons/seeMoreButton";
+import fetcher from "../utils/fetcher";
+import GridWebinars from "../components/webinars/gridWebinars";
+import FeaturedWebinars from "../components/webinars/featuredWebinars";
 
 export async function getServerSideProps() {
-    const URLBLOG = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_BLOG}?populate=Imagen_Destacada%2C%20autores.Perfiles%2C%20tags%2C%20categoria%2C%20categoria&filters[Destacada][$eq]=false&sort=fecha_publicacion:desc&pagination[page]=0&pagination[pageSize]=4`;
-    const URLBLOGDESTACADA = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_BLOG}?populate=Imagen_Destacada%2C%20autores.Perfiles%2C%20tags%2C%20categoria%2C%20categoria&filters[Destacada][$eq]=true&sort=fecha_publicacion:desc`;
-    const URLWEBINARS = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_WEBINARS}?populate=Imagen_Destacada%2C%20autores.Perfiles%2C%20tags%2C%20categoria&filters[Destacada][$eq]=false&sort=id:desc&pagination[page]=1&pagination[pageSize]=5`;
-    const URLWEBINARSDESTACADA = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_WEBINARS}?populate=Imagen_Destacada%2C%20autores.Perfiles%2C%20tags%2C%20categoria&filters[Destacada][$eq]=true&sort=id:desc&pagination[page]=1&pagination[pageSize]=5`;
-    const URLINSTITUCIONAL = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_INSTITUTIONAL}?populate[0]=Contacto&populate[1]=Assets.Logo_Alt`;
-    const URLFOOTERCONTENT = `${process.env.NEXT_PUBLIC_API}/page-web-layout?populate=deep&locale=es`;
+  const URLBLOG = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_BLOG}?populate=Imagen_Destacada%2C%20autores.Perfiles%2C%20tags%2C%20categoria%2C%20categoria&filters[Destacada][$eq]=false&sort=fecha_publicacion:desc&pagination[page]=0&pagination[pageSize]=4`;
+  const URLBLOGDESTACADA = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_BLOG}?populate=Imagen_Destacada%2C%20autores.Perfiles%2C%20tags%2C%20categoria%2C%20categoria&filters[Destacada][$eq]=true&sort=fecha_publicacion:desc`;
+  const URLWEBINARS = `${process.env.NEXT_PUBLIC_API}/blog-webinars?populate=deep&filters[webinarInfo][preWebinar][$eq]=false&pagination[page]=0&pagination[pageSize]=4`;
+  const URLWEBINARSDESTACADA = `${process.env.NEXT_PUBLIC_API}/blog-webinars?populate=deep&filters[webinarInfo][preWebinar][$eq]=true&pagination[page]=0&pagination[pageSize]=4`;
+  const URLINSTITUCIONAL = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_INSTITUTIONAL}?populate[0]=Contacto&populate[1]=Assets.Logo_Alt`;
+  const URLFOOTERCONTENT = `${process.env.NEXT_PUBLIC_API}/page-web-layout?populate=deep&locale=es`;
 
-    const resBlog = await fetch(URLBLOG,  {headers: {Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`}});
-    const dataBlog = await resBlog.json();
-    const dataNews = dataBlog;
+  const dataNotas = await fetcher(URLBLOG);
 
-    const resDestacada = await fetch(URLBLOGDESTACADA,  {headers: {Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`}});
-    const dataDestacada = await resDestacada.json();
-    const dataDes = dataDestacada;
+  const dataNotasDestacadas = await fetcher(URLBLOGDESTACADA);
 
-    const resWebinars = await fetch(URLWEBINARS,  {headers: {Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`}});
-    const dataWebinars = await resWebinars.json();
-    const dataWebin = dataWebinars;
+  const dataWebinars = await fetcher(URLWEBINARS);
 
-    const resWebinarsDestacada = await fetch(URLWEBINARSDESTACADA,  {headers: {Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`}});
-    const dataWebinarsDestacada = await resWebinarsDestacada.json();
-    const dataWebinDestacada = dataWebinarsDestacada;
+  const dataWebinarsInPreWebinar = await fetcher(URLWEBINARSDESTACADA);
 
-    const resInstitucional = await fetch(URLINSTITUCIONAL,  {headers: {Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`}});
-    const dataInstitucional = await resInstitucional.json();
-    const data = dataInstitucional;
-    
-    const resFooterContent = await fetch(URLFOOTERCONTENT,  {headers: {Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`}});
-    const dataFooter = await resFooterContent.json();
-    const footerContent = dataFooter;
+  const dataInstitucional = await fetcher(URLINSTITUCIONAL);
 
-    return { props: { dataDes, data, dataWebin, dataNews, dataWebinDestacada, footerContent } };
+  const footerContent = await fetcher(URLFOOTERCONTENT);
+
+  return {
+    props: {
+      dataNotasDestacadas,
+      dataInstitucional,
+      dataWebinars,
+      dataNotas,
+      dataWebinarsInPreWebinar,
+      footerContent,
+    },
+  };
 }
 
-export default function Home({ data, dataNews, dataDes, dataWebin, dataWebinDestacada, footerContent }) {
-    return (
-        <>
-            <Head>
-                <title>Blog Possumus </title>
-                <meta name="description" content="Blog de Possumus" />
-                <meta name="keywords" content="News, webinars, noticias, novedades, tecnologia, desarrollo" />
-                <meta name="author" content="Possumus" />
-                <meta property="og:title" content="Blog de possumus" key="title" />
-                <meta property="og:description" content="At Possumus, we implement Agile methodologies to create positive experiences through technology with human value. Software Engineering." />
-                <meta property="og:type" content="website" />
-                <meta property="og:site_name" content="Blog Possumus" />
-                <meta property="og:image" content="https://possumustech.blob.core.windows.net/staticfiles/assets/Possumus_d54fcb00ec.png"></meta>
-            </Head>
-            <Analitycs />
-            <Header title="Blog" style="mainNav" />
-            <Title title="Ultimas noticias" />
-            <FeaturedNews dataNews={dataDes.data} type="/news/"/>
-            <ListNews dataNews={dataNews.data} type="/news/" tag="Por " withSeeMoreButton/>
-            {/* <SeeMoreeButton link="/news" /> */}
-            <Title title="Webinars" />
-            <FeaturedNews dataNews={dataWebinDestacada.data} type="/webinars/" tag="Disertantes: " />
-            <ListNews dataNews={dataWebin.data} type="/webinars/" tag="Disertantes: " withSeeMoreButton/>
-            {/* <SeeMoreeButton link="/webinars" /> */}
-            <Footer dataInstitutional={data} footerContent={footerContent} />
-        </>
-    );
+export default function Home({
+  dataInstitucional,
+  dataNotas,
+  dataNotasDestacadas,
+  dataWebinars,
+  dataWebinarsInPreWebinar,
+  footerContent,
+}) {
+  return (
+    <>
+      <Head>
+        <title>Blog Possumus </title>
+        <meta name="description" content="Blog de Possumus" />
+        <meta
+          name="keywords"
+          content="News, webinars, noticias, novedades, tecnologia, desarrollo"
+        />
+        <meta name="author" content="Possumus" />
+        <meta property="og:title" content="Blog de possumus" key="title" />
+        <meta
+          property="og:description"
+          content="At Possumus, we implement Agile methodologies to create positive experiences through technology with human value. Software Engineering."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Blog Possumus" />
+        <meta
+          property="og:image"
+          content="https://possumustech.blob.core.windows.net/staticfiles/assets/Possumus_d54fcb00ec.png"
+        ></meta>
+      </Head>
+      <Analitycs />
+      <Header title="Blog" style="mainNav" />
+      <Title title="Ultimas noticias" />
+      <FeaturedNews dataNews={dataNotasDestacadas.data} type="/news/" />
+      <ListNews
+        dataNews={dataNotas.data}
+        type="/news/"
+        tag="Por "
+        withSeeMoreButton
+      />
+      <Title title="Webinars" />
+      {dataWebinarsInPreWebinar.data.length > 0 && (
+        // REFACTORIZAR
+        <FeaturedWebinars webinarsData={dataWebinarsInPreWebinar.data} />
+      )}
+      <GridWebinars webinars={dataWebinars.data} withSeeMoreButton />
+      <Footer
+        dataInstitutional={dataInstitucional}
+        footerContent={footerContent}
+      />
+    </>
+  );
 }
