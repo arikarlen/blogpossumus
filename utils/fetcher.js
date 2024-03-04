96d7e83dd1353e0a032f9fdfb06c59950ea9272c
Agustin Vera <agustin.vera@possumus.tech>
2024-03-04 15:13:24 -0300

    fix: refactoring fetcher to use Next revalidation

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,14 +1,17 @@
-const fetcher = async (url) => {
+//Por defecto la data se va a revaldiar cada 1 hora
+const fetcher = async (url, revalidate = 3600) => {
   try {
-    const response = await fetch(
-      `${url}`, {headers: {Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`}}
-    );
+    const response = await fetch(`${url}`, {
+      headers: { Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}` },
+      next: {revalidate: revalidate},
+    });
     const data = await response.json();
 
     return Promise.resolve(data);
   } catch (error) {
+    console.log(error)
     return Promise.reject(error);
   }
 };
 
-export default fetcher
+export default fetcher;
