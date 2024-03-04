28ff222dd86b7dd9e27b6fcbdfdd51b10c8be7f7
Agustin Vera <agustin.vera@possumus.tech>
2024-03-04 14:41:50 -0300

    fix: data fetching in news and restructuring

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,25 +1,11 @@
 import Head from "next/head";
-import fetcher from "../../utils/fetcher";
-import Header from "../../components/commons/header";
-import Footer from "../../components/commons/footer/footer";
 import SearchInput from "../../components/commons/searchInput/SearchInput";
-
-// import dynamic from "next/dynamic";
-// const SearchInput = dynamic('../../components/commons/searchInput/SearchInput')
-// const Header = dynamic('../../components/commons/header')
-// const Footer = dynamic('../../components/commons/footer/footer')
+import { getNews, nextPageNews, prevPageNews, setPageNews } from "./actions";
+import DataList from "@/components/commons/dataList/DataList";
+import PaginationBasic from "@/components/commons/paginationBasic";
 
 export default async function News() {
-  const newsURL = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_BLOG}?populate=*&pagination%5BwithCount%5D=true&pagination%5Bpage%5D=0&pagination%5BpageSize%5D=10&sort=id:desc`;
-  const institucionalURL = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_INSTITUTIONAL}?populate[0]=Contacto&populate[1]=Assets.Logo_Alt`;
-  const footerURL = `${process.env.NEXT_PUBLIC_API}/page-web-layout?populate=deep&locale=es`;
-
-  const {data: dataNews, meta: newsPagination} = await fetcher(newsURL);
-  const {data: dataInstitucional} = await fetcher(institucionalURL);
-  const {data: footerContent} = await fetcher(footerURL);
-
-  const {pagination} = newsPagination
-
+  const { dataNews, pagination, keyword, resultsNotFounded } = await getNews();
   return (
     <>
       <Head>
@@ -42,19 +28,19 @@ export default async function News() {
           content="https://possumustech.blob.core.windows.net/staticfiles/assets/Possumus_d54fcb00ec.png"
         ></meta>
       </Head>
-      <Header title="Blog" style="mainNav" />
-      {/* <Container>
-        <Breadcrumb>
-          <Breadcrumb.Item href="/">Inicio</Breadcrumb.Item>
-          <Breadcrumb.Item active href="/news">
-            News
-          </Breadcrumb.Item>
-        </Breadcrumb>
-      </Container> */}
-      <SearchInput initialData={dataNews} type="News" pagination={pagination} />
-      <Footer
-        dataInstitutional={dataInstitucional}
-        footerContent={footerContent}
+      <SearchInput type="News" />
+      <DataList
+        data={dataNews}
+        keyword={keyword}
+        isWebinar={false}
+        resultsNotFounded={resultsNotFounded}
+      />
+
+      <PaginationBasic
+        dataPagination={pagination}
+        prevPage={prevPageNews}
+        nextPage={nextPageNews}
+        setPage={setPageNews}
       />
     </>
   );
