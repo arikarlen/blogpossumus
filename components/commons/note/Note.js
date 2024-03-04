ce2f55c69612a6fec5aa626bb6d5735e39594c11
Agustin Vera <agustin.vera@possumus.tech>
2024-03-04 14:56:01 -0300

    fix: refactoring Note component using Tailwind

--- components/commons/note/Note.js
+++ components/commons/note/Note.js
@@ -1,28 +1,25 @@
-import moment from "moment";
 import Image from "next/image";
-import { Col } from "react-bootstrap";
 import styles from "./Note.module.css";
 import { TrimText } from "../../../utils/functions";
 import Link from "next/link";
+import Date from "../date/Date";
 
 export default function Note({ data }) {
   return (
-    <Col className={styles.note}>
-      <>
-        <Link href={`/news/${data.slug}`}>
-          <Image
-            src={data.Imagen_Destacada.data.attributes.url}
-            alt={data.Imagen_Destacada.data.attributes.name}
-            width="360"
-            height="190"
-          />
-          <h5>
-            {moment(data.fecha_publicacion).format("DD [de] MMMM [del] YYYY")}
-          </h5>
-          <h2>{data.Titulo}</h2>
-          {data.Bajada && <p>{TrimText(data.Bajada, 120)}</p>}
-        </Link>
-      </>
-    </Col>
+    <article className={styles.note}>
+      <Link href={`/news/${data.slug}`}>
+        <Image
+          src={data.Imagen_Destacada.data.attributes.url}
+          alt={data.Imagen_Destacada.data.attributes.name}
+          width={1920}
+          height={1080}
+        />
+        <h5>
+          <Date date={data.fecha_publicacion} />
+        </h5>
+        <h2>{data.Titulo}</h2>
+        {data.Bajada && <p>{TrimText(data.Bajada, 120)}</p>}
+      </Link>
+    </article>
   );
 }
