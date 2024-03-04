0a4135269e258477c1ff967b1835751201e505dd
Agustin Vera <agustin.vera@possumus.tech>
2024-03-04 14:56:15 -0300

    add: RecommendedArticles component

---
+++ components/commons/recomendedArticles/RecomendedArticles.js
@@ -0,0 +1,10 @@
+import FeaturedWebinars from "@/components/webinars/featuredWebinars/featuredWebinars";
+import Container from "../container/Container";
+import FeaturedNews from "@/components/news/featuredNews/featuredNews";
+
+export default function RecomendedArticles() {
+    return <Container className="grid grid-cols-2 justify-items-center text-start">
+        <FeaturedWebinars />
+        <FeaturedNews />
+    </Container>
+}
