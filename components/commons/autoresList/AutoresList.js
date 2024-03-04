13790fb60cceac0edcc75aac8f52b241451ad6ba
Agustin Vera <agustin.vera@possumus.tech>
2024-03-04 14:47:40 -0300

    fix: refactoring to handle autores name

--- components/commons/autoresList/AutoresList.js
+++ components/commons/autoresList/AutoresList.js
@@ -1,18 +1,39 @@
 export default function AutoresList({ autores }) {
+  const lastAuthorAndPenultimateAuthor = (id) => {
+    const lastPosition = autores.length - 1;
+    return {
+      lastAuthor: lastPosition === id,
+      penultimateAuthor: lastPosition - 1 === id,
+    };
+  };
+
+  const soleAuthor = autores.length === 1;
   return (
     <>
-      {autores?.map((autor, idx) => (
-        <a
-          href={autor.attributes?.Perfiles?.Principal}
-          target="_blank"
-          className="linkPerfil"
-          key={`${autor.attributes?.Nombre}-${idx}`}
-        >
-          {autores.length - 1 === idx
-            ? autor.attributes?.Nombre
-            : `${autor.attributes?.Nombre}, `}
-        </a>
-      ))}
+      {autores?.map((autor, idx) => {
+        const { lastAuthor, penultimateAuthor } =
+          lastAuthorAndPenultimateAuthor(idx);
+        return (
+          <a
+            href={autor.attributes?.Perfiles?.Principal}
+            target="_blank"
+            className="linkPerfil"
+            key={`${autor.attributes?.Nombre}-${idx}`}
+          >
+            {soleAuthor
+              ? `${autor.attributes?.Nombre}`
+              : lastAuthor
+              ? ` ${
+                  autor.attributes?.Nombre.charAt(0).toLowerCase() === "i"
+                    ? "e"
+                    : "y"
+                } ${autor.attributes?.Nombre}`
+              : penultimateAuthor
+              ? `${autor.attributes?.Nombre}`
+              : `${autor.attributes?.Nombre}, `}
+          </a>
+        );
+      })}
     </>
   );
 }
