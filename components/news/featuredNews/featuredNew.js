14e19935bcea73fb4c5fdef58e78df746249865f
Agustin Vera <agustin.vera@possumus.tech>
2024-03-06 14:30:12 -0300

    add: get data from server actions & using suspense

--- components/news/featuredNews/featuredNew.js
+++ components/news/featuredNews/featuredNew.js
@@ -1,12 +1,15 @@
-"use client";
 import AutoresList from "@/components/commons/autoresList/AutoresList";
 import Date from "@/components/commons/date/Date";
 import Title from "@/components/commons/titles";
+import { fetchFeatured } from "app/actions";
 import Head from "next/head";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
-export default function FeaturedNew({ data }) {
-  const router = useRouter();
+export default async function FeaturedNew() {
+  const URLBLOGDESTACADA = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_BLOG}?populate=Imagen_Destacada%2C%20autores.Perfiles%2C%20tags%2C%20categoria%2C%20categoria&filters[Destacada][$eq]=true&sort=fecha_publicacion:desc`;
+  const data = await fetchFeatured(URLBLOGDESTACADA, 3600);
+
+  if(!data) return
 
   const imagenDestacada = `${data.attributes.Imagen_Destacada?.data.attributes.url}?q=70`;
   return (
@@ -16,43 +19,34 @@ export default function FeaturedNew({ data }) {
         <link rel="dns-prefetch" href="https://adminpss.possumus.tech/" />
         <link rel="preload" href={imagenDestacada} as="image" />
       </Head>
-      <article
-        className="hover:cursor-pointer"
-        key={data.attributes.slug}
-        onClick={() => router.push(`/news/${data.attributes.slug}`)}
-      >
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          width={1920}
-          height={1080}
-          src={imagenDestacada}
-          alt={data.attributes.Titulo}
-          loading="lazy"
-          onClick={() => router.push(`/news/${data.attributes.slug}`)}
-        />
-        <div className="textContent">
-          <h6
-            className="linkPerfil"
-            onClick={() =>
-              router.push(
-                "/category/" +
-                  data.attributes.categoria?.data.attributes.Categoria
-              )
-            }
-          >
-            {data.attributes.categoria?.data.attributes.Categoria}
-          </h6>
-          <Title
-            title={data.attributes.Titulo.replaceAll("#", "")}
-            className="mb-2"
-            fluid
-          />
-          <h5>
-            <Date date={data.attributes.publishedAt} /> | Por{" "}
-            <AutoresList autores={data.attributes.autores.data} />
-          </h5>
-          <p>{data.attributes.Bajada}</p>
-        </div>
+      <article className="hover:cursor-pointer" key={data.attributes.slug}>
+        <Link href={`/news/${data.attributes.slug}`}>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              width={1920}
+              height={1080}
+              src={imagenDestacada}
+              alt={data.attributes.Titulo}
+              loading="lazy"
+            />
+            <div className="textContent">
+              <h6 className="linkPerfil">
+                {data.attributes.categoria?.data.attributes.Categoria}
+              </h6>
+              <Title
+                title={data.attributes.Titulo.replaceAll("#", "")}
+                className="mb-2"
+                fluid
+              />
+              <h5>
+                <Date date={data.attributes.publishedAt} /> | Por{" "}
+                <AutoresList autores={data.attributes.autores.data} />
+              </h5>
+              <p>{data.attributes.Bajada}</p>
+            </div>
+          </div>
+        </Link>
       </article>
     </>
   );
--- components/news/featuredNews/featuredNews.js
+++ components/news/featuredNews/featuredNews.js
@@ -1,20 +1,16 @@
-import fetcher from "utils/fetcher";
+import { Suspense } from "react";
 import Container from "../../commons/container/Container";
 import FeaturedNew from "./featuredNew";
+import FeaturedUI from "@/components/skeletons/FeaturedUI";
 
 export default async function FeaturedNews() {
-  const URLBLOGDESTACADA = `${process.env.NEXT_PUBLIC_API}/${process.env.NEXT_PUBLIC_API_BLOG}?populate=Imagen_Destacada%2C%20autores.Perfiles%2C%20tags%2C%20categoria%2C%20categoria&filters[Destacada][$eq]=true&sort=fecha_publicacion:desc`;
-  const { data: dataNotasDestacadas } = await fetcher(URLBLOGDESTACADA, 3600);
-
   return (
     <>
-      {dataNotasDestacadas.length > 0 && (
+      <Suspense fallback={<FeaturedUI />}>
         <Container>
-          {dataNotasDestacadas?.map((data, idx) => (
-            <FeaturedNew data={data} key={idx} />
-          ))}
+          <FeaturedNew />
         </Container>
-      )}
+      </Suspense>
     </>
   );
 }
