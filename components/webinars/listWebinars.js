3efafaa7ee8b7932d3b8e3ecb2e3240112b36054
Agustin Vera <agustin.vera@possumus.tech>
2024-03-06 15:40:36 -0300

    add: dataList using Suspense & server components

--- components/webinars/listWebinars.js
+++ components/webinars/listWebinars.js
@@ -1,27 +1,30 @@
-"use client"
-import Container from "@/components/commons/container/Container"
+import Container from "@/components/commons/container/Container";
 import AutoresList from "@/components/commons/autoresList/AutoresList";
 import Title from "@/components/commons/titles";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Date from "../commons/date/Date";
+import Link from "next/link";
+import { getWebinars } from "app/webinars/actions";
 
-export default function ListWebinars({ webinarsData, title }) {
-  const router = useRouter();
+export default async function ListWebinars() {
+  const {dataWebinars} = await getWebinars()
   return (
     <Container>
-      <h4>{title}</h4>
-
-      {webinarsData?.map(({ attributes }) => {
+      {dataWebinars?.map(({ attributes }) => {
         const { webinarInfo, header, autores } = attributes;
         return (
           <article
             className="flex flex-col md:flex-row gap-8 justify-between items-center border-b border-b-gray-d8 pb-3 cursor-pointer md:hover:scale-101 md:hover:shadow-md duration-300 ease-in-out"
-            onClick={() => router.push(`/webinars/${webinarInfo.slug}`)}
             key={webinarInfo?.slug}
           >
             <div>
-              <Title title={header?.titulo.replaceAll("#", "")} className="mt-6 mb-2" fluid/>
+              <Link href={`/webinars/${webinarInfo.slug}`}>
+                <Title
+                  title={header?.titulo.replaceAll("#", "")}
+                  className="mt-6 mb-2"
+                  fluid
+                />
+              </Link>
               {/* se remplazan los # porque este campo viene desde un tipo ricktext en Strapi */}
               <h5>
                 <Date date={header?.fecha} /> | Por{" "}
@@ -31,12 +34,14 @@ export default function ListWebinars({ webinarsData, title }) {
               <p>{header?.bajada}</p>
             </div>
             <div className="md:max-w-60">
-              <Image
-                src={webinarInfo?.image?.data?.attributes.url}
-                alt={header?.titulo}
-                width={1080}
-                height={532}
-              />
+              <Link href={`/webinars/${webinarInfo.slug}`}>
+                <Image
+                  src={webinarInfo?.image?.data?.attributes.url}
+                  alt={header?.titulo}
+                  width={1080}
+                  height={532}
+                />
+              </Link>
             </div>
           </article>
         );
