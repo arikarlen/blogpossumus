12f047a3e7b1a9ded55abb432d3519491ac65ce2
Agustin Vera <agustin.vera@possumus.tech>
2024-03-26 11:49:10 -0300

    fix:using useParam to handle lang in useDictionary

--- hooks/useDictionary.js
+++ hooks/useDictionary.js
@@ -3,8 +3,9 @@ import enDictionary from "../dictionaries/en.json";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function useDictionary(lang) {
+export default function useDictionary() {
   const [dictionary, setDictionary] = useState(esDictionary);
+  const { lang } = useParams();
   useEffect(() => {
     if (lang === "en") {
       setDictionary(enDictionary);
