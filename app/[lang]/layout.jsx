d5c7bbe565ff5e21310bd87f47a85acbe8836424
Agustin Vera <agustin.vera@possumus.tech>
2024-03-26 11:45:27 -0300

    fix: move not-found to /app, add: langs in sitemap

--- app/[lang]/layout.jsx
+++ app/[lang]/layout.jsx
@@ -1,4 +1,3 @@
-import "../../styles/index.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import Header from "../../components/commons/header";
--- app/[lang]/not-found.jsx
+++
@@ -1,3 +0,0 @@
-export default function NotFound(){
-    return <h3>404</h3>
-}
